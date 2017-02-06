3443701862d2d4f13ed5398284ea532229beef5c
Caleb L <caleb.lenoir@gmail.com>
2017-02-06 10:00:35 -0500

    add deploy script for mup

---
+++ .deploy/mup.js
@@ -0,0 +1,37 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '138.197.84.191',
+      username: 'root'
+      // pem:
+      // password:
+      // or leave blank for authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'app',
+    path: '../app',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'app.com',
+      MONGO_URL: 'mongodb://localhost/meteor'
+    },
+
+    //dockerImage: 'kadirahq/meteord'
+    deployCheckWaitTime: 60
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
